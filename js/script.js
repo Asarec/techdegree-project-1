e07ecb1baab7ece9ab7f415eb2e06f2eca1a7fa6
Alfredo Garcia <Asarec@users.noreply.github.com>
2020-06-21 10:14:31 -0400

    Added array of quotes

--- js/script.js
+++ js/script.js
@@ -1,33 +1,46 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+// Array of Quotes
+const quotes = [
+  {
+    quote: "We accept the love we think we deserve.",
+    source: "Stephen Chbosky",
+    citation: "The Perks of Being a Wallflower",
+    year: 1999,
+    tags: ["inspirational", "love"]
+  },
+  {
+    quote: "Without music, life would be a mistake.",
+    source: "Friedrich Nietzsche",
+    citation: "Twilight of the Idols.",
+    year: 1889,
+    tags: ["inspirational", "music", "philosophy"]
+  },
+  {
+    quote: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
+    source: "Martin Luther King Jr.",
+    citation: "A Testament of Hope: The Essential Writings and Speeches",
+    year: 1986,
+    tags: ["inspirational", "light", "peace"]
+  },
+  {
+    quote: "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.",
+    source: "H. Jackson Brown Jr.",
+    citation: "P.S. I Love You",
+    year: 2003,
+    tags: ["dreams", "action"]
+  },
+  {
+    quote: "It is better to be hated for what you are than to be loved for what you are not.",
+    source: "Andre Gide",
+    citation: "Autumn Leaves",
+    year: 1950,
+    tags: ["life", "love"]
+  }
+];
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
-
-
-/***
- * `getRandomQuote` function
-***/
-
-
-
-/***
- * `printQuote` function
-***/
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
-
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// Output Test
+console.log(quotes[0]['quote']);
+console.log(quotes[0]['source']);
+console.log(quotes[0]['citation']);
+console.log(quotes[0]['year']);
+console.log(quotes[0]['tags'][0]);
+console.log(quotes[0]['tags'][1]);

6064e32322a80933a7842e4c8de855468c6bf121
Alfredo Garcia <Asarec@users.noreply.github.com>
2020-06-23 05:56:57 -0400

    Moved quotes to its own js file.

---
+++ js/quotes.js
@@ -0,0 +1,46 @@
+/**
+ *  Array of quotes for randomized output.
+ *  @param {Object[]} quotes            - List of quotes used for this script.
+ *  @param {string} quotes[].quote      - The quote itself.
+ *  @param {string} quotes[].source     - The author of the quote.
+ *  @param {string} quotes[].citation   - Citation information for the quote.
+ *  @param {number} quotes[].number     - Year the quote was created.
+ *  @param {Array} quotes[].tags        - Tags describing the quote.
+ */
+const quotes = [
+	{
+		quote: "We accept the love we think we deserve.",
+		source: "Stephen Chbosky",
+		citation: "The Perks of Being a Wallflower",
+		year: 1999,
+		tags: ["inspirational", "love"]
+	},
+	{
+		quote: "Without music, life would be a mistake.",
+		source: "Friedrich Nietzsche",
+		citation: "Twilight of the Idols.",
+		year: 1889,
+		tags: ["inspirational", "music", "philosophy"]
+	},
+	{
+		quote: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
+		source: "Martin Luther King Jr.",
+		citation: "A Testament of Hope: The Essential Writings and Speeches",
+		year: 1986,
+		tags: ["inspirational", "light", "peace"]
+	},
+	{
+		quote: "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.",
+		source: "H. Jackson Brown Jr.",
+		citation: "P.S. I Love You",
+		year: 2003,
+		tags: ["dreams", "action"]
+	},
+	{
+		quote: "It is better to be hated for what you are than to be loved for what you are not.",
+		source: "Andre Gide",
+		citation: "Autumn Leaves",
+		year: 1950,
+		tags: ["life", "love"]
+	}
+];

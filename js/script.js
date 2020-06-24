a21ade49b98041fa4916449753191dd7991eb524
Alfredo Garcia <Asarec@users.noreply.github.com>
2020-06-24 15:07:57 -0400

    Additional fixes and new quote for variety.

--- js/script.js
+++ js/script.js
@@ -15,29 +15,30 @@ const quotes = [
         tags: ['inspirational', 'music', 'philosophy'],
     },
     {
-        quote:
-            'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.',
+        quote: 'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.',
         source: 'Martin Luther King Jr.',
         citation: 'A Testament of Hope: The Essential Writings and Speeches',
         year: 1986,
         tags: ['inspirational', 'light', 'peace'],
     },
     {
-        quote:
-            "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.",
+        quote: 'Twenty years from now you will be more disappointed by the things that you didn\'t do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.',
         source: 'H. Jackson Brown Jr.',
         citation: 'P.S. I Love You',
         year: 2003,
         tags: ['dreams', 'action'],
     },
     {
-        quote:
-            'It is better to be hated for what you are than to be loved for what you are not.',
+        quote: 'It is better to be hated for what you are than to be loved for what you are not.',
         source: 'Andre Gide',
         citation: 'Autumn Leaves',
         year: 1950,
         tags: ['life', 'love'],
     },
+    {
+        quote: 'Be yourself; everyone else is already taken.',
+        source: 'Oscar Wilde'
+    }
 ];
 
 /** Returns the object of a random quote. */
